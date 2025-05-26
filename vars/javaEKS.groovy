4dae02102b45d83c42e51c432d6aad9e34d6c5c8
nnreddybalreddy <narendra.mahn@gmail.com>
2025-05-26 13:40:38 -0400

    ALL

--- vars/javaEKS.groovy
+++ vars/javaEKS.groovy
@@ -61,7 +61,7 @@ pipeline {
                     aws eks update-kubeconfig --region us-east-1 --name expense-dev
                     cd helm
                     sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                    helm upgrade --install backend .
+                    helm upgrade --install  ${component} -n expense .
                 """
             }
         }
